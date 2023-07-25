d38aeb5466734713c54772be5ca4221f282ee996
chiida0243 <ririka.n.n.0024@gmail.com>
2023-07-25 18:27:02 +0900

    投稿処理実行

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,19 +7,19 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body class="antialiased">
-        <h1>Blig Name</h1>
-       <form action="/posts" method="POST">
+        <h1>Blog Name</h1>
+        <form action="/posts" method="POST">
            @csrf
-           <div class="title">
+            <div class="title">
                <h2>Title</h2>
-               <input type="text" name=post[title] placeholder="タイトル">
-           </div>
-           <div class="body">
+               <input type="text" name="post[title]" placeholder="タイトル">
+            </div>
+            <div class="body">
                <h2>Body</h2>
                <textarea name="post[body]" placeholder="今日も一日お疲れさまでした。"></textarea>
            </div>
            <input type="submit" value="store">
-       </form>
+        </form>
         <div class='footer'>
             <a href="/">戻る</a>
         </div>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,8 +3,10 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
+        
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    
     </head>
     <body class="antialiased">
         <h1>Blog Name</h1>
