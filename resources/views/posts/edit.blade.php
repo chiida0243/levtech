5b3e13f3efe2b3be0f890f0ef392789a8b6524cd
chiida0243 <ririka.n.n.0024@gmail.com>
2023-07-26 01:12:35 +0900

    編集画面

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1  class="title">Edit</h1>
+        <div class="content">
+        <form action="/posts/ {{ $post->id }}"method="POST">
+           @csrf
+           @method('PUT')
+            <div class='content__title'>
+               <h2>Title</h2>
+               <input type="text" name="post[title]" value={{ $post->title }}>
+               <p class='title__error' style="color:red">{{ $errors->first('post.title') }}</p>
+            </div>
+            <div class='content__body'>
+               <h2>Body</h2>
+               <input type='text' name="post[body]" value="{{ $post->body }}">
+           </div>
+           <input type="submit" value="update">
+        </form>
+        </div>
+    </body>
+</html>
